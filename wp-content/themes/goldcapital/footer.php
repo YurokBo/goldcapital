894f535a87a5d024708ff7008cba1ba7efe43635
Yuriy Bondaryuk <vertel3012@mail.ru>
2021-09-15 21:28:01 +0300

    main fixed link

--- wp-content/themes/goldcapital/footer.php
+++ wp-content/themes/goldcapital/footer.php
@@ -230,7 +230,7 @@
                     </div>
                 </li>
             </div>
-            <a href="https://gobymylink.com/ru/?cpa_partner_id=12555567&sub_id=GoldCapital" target="_blank"
+            <a href="https://gobymylink.com/ru/registration/?cpa_partner_id=12555567&sub_id=GoldCapital" target="_blank"
                rel="nofollow noopener noreferrer" class="nav-btn menu-collapse-btn btn ">
                 Регистрация
             </a>
