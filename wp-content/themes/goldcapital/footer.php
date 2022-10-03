49c3f06e6e7be7fabe6b39e321c9ae1f41ade0f5
Yuriy Bondaryuk <ybondaryuk@marfa-tech.com>
2022-10-03 18:59:40 +0300

    main text in footer

--- wp-content/themes/goldcapital/footer.php
+++ wp-content/themes/goldcapital/footer.php
@@ -82,7 +82,8 @@
         <div class="row align-items-center">
             <div class="col-md-4">
                 <a href="#" class="footer-logo">
-                    <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/footer-logo.png" alt="GoldCapital" class="footer-logo__img img">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/img/footer-logo.png" alt="GoldCapital"
+                         class="footer-logo__img img">
                 </a>
             </div>
             <div class="col-md-8 ">
@@ -125,23 +126,30 @@
                 </ul>
             </div>
         </div>
+        <p class="row align-items-center">
+            Начиная работать на финансовых рынках, убедитесь, что вы осознаете риски, с которыми сопряжена торговля с
+            использованием кредитного плеча, и что вы имеете достаточный уровень подготовки.
+        </p>
     </div>
 </footer>
 <!-- Modal window -->
 <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-        <div class="modal-content" style="background-image: url(<?php bloginfo( 'template_url' ); ?>/assets/img/modal-body-bg.jpg)">
+        <div class="modal-content"
+             style="background-image: url(<?php bloginfo('template_url'); ?>/assets/img/modal-body-bg.jpg)">
             <div class="modal-header">
 
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/close-btn.png" alt="close">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/img/close-btn.png" alt="close">
                 </button>
             </div>
             <div class="modal-body">
-                <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/modal-icon.png" alt="modal-icon" class="modal__icon">
+                <img src="<?php bloginfo('template_url'); ?>/assets/img/modal-icon.png" alt="modal-icon"
+                     class="modal__icon">
                 <div class="modal__title">Спасибо за Вашу заявку!</div>
                 <div class="modal__text">Менеджер свяжется с Вами в ближайшее время</div>
-                <div class="modal-social__box col-xl-2 col-lg-4" style="background-image: url(<?php bloginfo( 'template_url' ); ?>/assets/img/model-border.svg)">
+                <div class="modal-social__box col-xl-2 col-lg-4"
+                     style="background-image: url(<?php bloginfo('template_url'); ?>/assets/img/model-border.svg)">
                     <ul class="modal-social d-flex">
                         <li class="social__title">
                             Следите за новостями!
@@ -169,7 +177,8 @@
     <ul class="menu-collapse__list d-flex ">
         <a href="#" class="menu-collapse-logo mr-auto">
 
-            <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/menu-collapse-logo.png" alt="GoldCapital" class="menu-collapse-logo__img img">
+            <img src="<?php bloginfo('template_url'); ?>/assets/img/menu-collapse-logo.png" alt="GoldCapital"
+                 class="menu-collapse-logo__img img">
 
         </a>
         <li class="menu-collapse__item">
